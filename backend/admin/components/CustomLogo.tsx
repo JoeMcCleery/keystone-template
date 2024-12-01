5f5e5d50b5275b8434dd6cbe81beb38af152d06c
JoeMcCleery <mccleery.joseph@gmail.com>
2024-12-01 21:42:21 +0800

    add custom logo and navigation to admin dashboard

---
+++ backend/admin/components/CustomLogo.tsx
@@ -0,0 +1,3 @@
+export default function CustomLogo() {
+  return <h3>Keystone Template</h3>
+}
---
+++ backend/admin/components/CustomNavigation.tsx
@@ -0,0 +1,11 @@
+import type { NavigationProps } from '@keystone-6/core/admin-ui/components'
+import { NavigationContainer, ListNavItems, NavItem } from '@keystone-6/core/admin-ui/components'
+
+export default function CustomNavigation({ lists, authenticatedItem }: NavigationProps) {
+  return (
+    <NavigationContainer authenticatedItem={authenticatedItem}>
+      <NavItem href="/">Dashboard</NavItem>
+      <ListNavItems lists={lists} />
+    </NavigationContainer>
+  )
+}
