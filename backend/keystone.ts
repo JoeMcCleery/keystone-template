a201b5598f5cb22aad73ffbedf5914b3df9d37f4
JoeMcCleery <mccleery.joseph@gmail.com>
2024-12-01 14:26:29 +0800

    use postgres for keystone db

--- backend/keystone.ts
+++ backend/keystone.ts
@@ -17,11 +17,8 @@ import { withAuth, session } from './auth'
 export default withAuth(
   config({
     db: {
-      // we're using sqlite for the fastest startup experience
-      //   for more information on what database might be appropriate for you
-      //   see https://keystonejs.com/docs/guides/choosing-a-database#title
-      provider: 'sqlite',
-      url: 'file:./keystone.db',
+      provider: 'postgresql',
+      url: 'postgres://admin:root@db:5432/keystone',
     },
     lists,
     session,
